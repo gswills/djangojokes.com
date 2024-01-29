c351f06f441fafe64a4e445c340698da12f691a4
gswills <gregwills@live.com>
2024-01-29 10:00:07 -0500

    Through L9-T9

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Joke
+from .models import Category, Joke
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['category', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
 
 @admin.register(Joke)
 class JokeAdmin(admin.ModelAdmin):
@@ -11,5 +21,4 @@ class JokeAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
             return ('slug', 'created', 'updated')
-
         return ()

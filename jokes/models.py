dfa5871aa956772275a1d154ab621cbaf254166e
gswills <gregwills@live.com>
2024-02-02 19:14:37 -0500

    Through L10-T15

--- jokes/models.py
+++ jokes/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
+
 
 from common.utils.text import unique_slug
 
@@ -36,6 +38,9 @@ class Category(models.Model):
 class Joke(models.Model): 
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT
+    )
     category = models.ForeignKey(
         Category, on_delete=models.PROTECT
     )
--- jokes/views.py
+++ jokes/views.py
@@ -14,6 +14,9 @@ class JokeCreateView(CreateView):
     model = Joke
     form_class = JokeForm
   
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class JokeDeleteView(DeleteView):
